eaeb295893be42813458ef8eb8416a8e775e9b9d
Kamil <kamil.suwiczak@wp.pl>
2024-04-22 14:30:45 +0200

    dodane menu

--- cw2/data/main.py
+++ cw2/data/main.py
@@ -3,28 +3,40 @@ import sys
 
 x= True
 y=[]
+action =""
+
 #Obsługa BST podawanego jako python3 main.py --tree BST 16 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17, gdzie 16 to liczba węzłów
 if sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)>3:
     for i in range (0, int(sys.argv[3])):
         y.append(int(sys.argv[i+4]))
 # Obsługa BST podawanego do programu jako node> i insert>
 elif sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)<=3:
-    x=input("nodes>")
-    a=input("insert>")
+    x=input("nodes> ")
+    a=input("insert >")
     for i in range(0, len(a)):
         if a[i]==" ":
             continue
         y.append(int(a[i]))
 # print(len(sys.argv))
 z=trees.BST(y)
-print(z.print())
+# print(z.print())
 
-# while x:
-#     print('''wybierz opcje: 
-#           1. Wprowadz drzewo\n
-#           2. Zakończ \n''')
-#     y=input(">>>")
-#     if int(y) == 2:
-#         x= False
+#menu 
+while(x):
+    action = input("action> ")
+    match action:
+        case "exit":
+            x= False
+        case  "print":
+            print(z.print())
+        case "help":
+            print("help         Show this message")
+            print("print        Print the tree using in-order, pre-order, post-order")
+            print("remove       Remove elements of the tree")
+            print("delete       Delete the whole tree")
+            print("export       Export the tree to tickzpicture")
+            print("rebalance    Rebalance the tree")
+            print("exit         Exits the program (the same as ctrl + D)")
 
-
+        
+    
