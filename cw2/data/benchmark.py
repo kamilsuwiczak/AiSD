f9aa7585d83744cd6c39a07a049610d89f15619b
Kamil <kamil.suwiczak@wp.pl>
2024-04-24 00:35:36 +0200

    Added mesuring time of creating BST

--- cw2/data/benchmark.py
+++ cw2/data/benchmark.py
@@ -2,11 +2,10 @@ import sys
 import os
 import trees
 import time
-import string
 
 
 def measure_time_of_creating_AVL():
-    f1 = open("benchmark_results/results.txt", "w")
+    f1 = open("benchmark_results/results.txt", "a")
     f1.write("Action, number_of_elements, time\n")
     files = os.listdir("benchmark_data/random")
     for i in files:
@@ -22,7 +21,28 @@ def measure_time_of_creating_AVL():
             tree = trees.AVL(tab)
             finish_time=time.time()-start_time
             finish_time = str(finish_time).replace(".", ",")
-            f2.write(f"creating AVL, {i}, {finish_time}\n")
+            f1.write(f"creating AVL, {i}, {finish_time}\n")
+        tab.clear()
+    f1.close()
+
+def measure_time_of_creating_BST():
+    f1 = open("benchmark_results/results.txt", "a")
+    f1.write("Action, number_of_elements, time\n")
+    files = os.listdir("benchmark_data/random")
+    for i in files:
+        tab=[]
+        f2=open(f"benchmark_data/random/{i}", "r")
+        x = int(f2.readline())
+        for j in range(0, x):
+            tab.append(int(f2.readline()))
+        f2.close()
+
+        for k in range(0,4):
+            start_time = time.time()
+            tree = trees.BST(tab)
+            finish_time=time.time()-start_time
+            finish_time = str(finish_time).replace(".", ",")
+            f1.write(f"creating BST, {i}, {finish_time}\n")
         tab.clear()
     f1.close()
 
@@ -44,9 +64,13 @@ def test_print():
 def main():
     if os.path.exists("benchmark_results") == False:
         os.mkdir("benchmark_results")
+    if os.path.exists("benchmark_results/results.txt") == False:
+        file=open("benchmark_results/results.txt", "x")
+        file.close()
     # print(printing_from_files())
     # test_print()
     measure_time_of_creating_AVL()
+    measure_time_of_creating_BST()
     # files = os.listdir("benchmark_data/random")
     # dx = trees.AVL([8,2,5,14,10,12,13,6,9,1,4])
     # start_time = time.time()
