817889b3d2ef74ce9e49da6f0c9fd07ed57ecd3d
Kamil <kamil.suwiczak@wp.pl>
2024-04-23 12:34:06 +0200

    Dodana obsługa AVL

--- cw2/data/main.py
+++ cw2/data/main.py
@@ -2,14 +2,14 @@ import trees
 import sys
 
 x= True
-y=[]
+bst_data=[]
 action =""
 
 #Obsługa BST podawanego jako python3 main.py --tree BST 16 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17, gdzie 16 to liczba węzłów
 if sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)>3:
     for i in range (0, int(sys.argv[3])):
-        y.append(int(sys.argv[i+4]))
-
+        bst_data.append(int(sys.argv[i+4]))
+    z=trees.BST(bst_data)
 # Obsługa BST podawanego do programu jako node> i insert>
 elif sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)<=3:
     nodes=input("nodes> ")
@@ -17,9 +17,13 @@ elif sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)<=3:
     for i in range(0, len(a)):
         if a[i]==" ":
             continue
-        y.append(int(a[i]))
-# print(len(sys.argv))
-z=trees.BST(y)
+        bst_data.append(int(a[i]))
+    z=trees.BST(bst_data)
+#obsługa AVL podawanego do programu jako python3 main.py --tree AVL 11 8 2 5 14 10 12 13 6 9 1 4, gdzie 11 to liczba węzłów
+elif sys.argv[1] =="--tree" and sys.argv[2] == "AVL" and len(sys.argv)>3:
+    for i in range (0, int(sys.argv[3])):
+        bst_data.append(int(sys.argv[i+4]))
+    z=trees.AVL(bst_data)
 # print(z.print())
 
 #menu 
@@ -43,16 +47,16 @@ while(x):
             print("post-order: ", tab[2])
 
         case "remove":
-            tab2=[]
+            tab=[]
             tmp=""
             nodes_to_remove = input("remove> ")
             for i in range (0, len(nodes_to_remove)):
                 if nodes_to_remove[i] != " ":
                     tmp = tmp+nodes_to_remove[i]
                 if nodes_to_remove[i] == " " or i == len(nodes_to_remove)-1:
-                    tab2.append(tmp)
+                    tab.append(tmp)
                     tmp = ""
-            for i in tab2:
+            for i in tab:
                 z.delete(int(i))
 
         case "delete":
