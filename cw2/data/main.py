4b783f9dcb31e83078e605b4a13df3fe4e0a7fd2
Kamil <kamil.suwiczak@wp.pl>
2024-04-24 22:47:24 +0200

    Updated main.py added here string dealing

--- cw2/data/main.py
+++ cw2/data/main.py
@@ -1,77 +1,114 @@
 import trees
 import sys
 
-x= True
-bst_data=[]
-action =""
+tree_data=[]
+if sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)==3:
+    print(len(sys.argv))
+    x=input()
+    x= x.split()
+    for i in range (0, int(x.pop(0))):
+        tree_data.append(int(x.pop(0)))
+    z=trees.BST(tree_data)
+    for i in x:
+        action = i
+        match action:
+            case "help":
+                print("help         Show this message")
+                print("print        Print the tree using in-order, pre-order, post-order")
+                print("remove       Remove elements of the tree")
+                print("delete       Delete the whole tree")
+                print("export       Export the tree to tickzpicture")
+                print("rebalance    Rebalance the tree")
+                print("findMinMax   Finding minimum and maximum in tree")
+                print("exit         Exits the program (the same as ctrl + D)")
 
-#Obsługa BST podawanego jako python3 main.py --tree BST 16 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17, gdzie 16 to liczba węzłów
-if sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)>3:
-    for i in range (0, int(sys.argv[3])):
-        bst_data.append(int(sys.argv[i+4]))
-    z=trees.BST(bst_data)
-# Obsługa BST podawanego do programu jako node> i insert>
-elif sys.argv[1] =="--tree" and sys.argv[2] == "BST" and len(sys.argv)<=3:
+            case  "print":
+                tab = z.print()
+                print("pre-order: ", tab[0])
+                print("In-order: ", tab[1])
+                print("post-order: ", tab[2])
+
+            case "remove":
+                tab=[]
+                tmp=""
+                nodes_to_remove = input("remove> ")
+                for i in range (0, len(nodes_to_remove)):
+                    if nodes_to_remove[i] != " ":
+                        tmp = tmp+nodes_to_remove[i]
+                    if nodes_to_remove[i] == " " or i == len(nodes_to_remove)-1:
+                        tab.append(tmp)
+                        tmp = ""
+                for i in tab:
+                    z.delete(int(i))
+
+            case "delete":
+                z.del_all()
+                print("Tree has been deleted")
+
+            case "export":
+                pass
+
+            case "rebalance":
+                pass
+            case "findMinMax":
+                print("Min: ", z.find_min())
+                print("Max", z.find_max())
+            case "exit":
+                pass
+
+elif sys.argv[1] =="--tree" and sys.argv[2] == "BST" and sys.argv[3]=="hand":
     nodes=input("nodes> ")
     a=input("insert >")
     for i in range(0, len(a)):
         if a[i]==" ":
             continue
-        bst_data.append(int(a[i]))
-    z=trees.BST(bst_data)
-#obsługa AVL podawanego do programu jako python3 main.py --tree AVL 11 8 2 5 14 10 12 13 6 9 1 4, gdzie 11 to liczba węzłów
-elif sys.argv[1] =="--tree" and sys.argv[2] == "AVL" and len(sys.argv)>3:
-    for i in range (0, int(sys.argv[3])):
-        bst_data.append(int(sys.argv[i+4]))
-    z=trees.AVL(bst_data)
-# print(z.print())
-
-#menu 
-while(x):
-    action = input("action> ")
-    match action:
-        case "help":
-            print("help         Show this message")
-            print("print        Print the tree using in-order, pre-order, post-order")
-            print("remove       Remove elements of the tree")
-            print("delete       Delete the whole tree")
-            print("export       Export the tree to tickzpicture")
-            print("rebalance    Rebalance the tree")
-            print("findMinMax   Finding minimum and maximum in tree")
-            print("exit         Exits the program (the same as ctrl + D)")
-
-        case  "print":
-            tab = z.print()
-            print("pre-order: ", tab[0])
-            print("In-order: ", tab[1])
-            print("post-order: ", tab[2])
-
-        case "remove":
-            tab=[]
-            tmp=""
-            nodes_to_remove = input("remove> ")
-            for i in range (0, len(nodes_to_remove)):
-                if nodes_to_remove[i] != " ":
-                    tmp = tmp+nodes_to_remove[i]
-                if nodes_to_remove[i] == " " or i == len(nodes_to_remove)-1:
-                    tab.append(tmp)
-                    tmp = ""
-            for i in tab:
-                z.delete(int(i))
-
-        case "delete":
-            z.del_all()
-            print("Tree has been deleted")
-
-        case "export":
-            pass
-
-        case "rebalance":
-            pass
-        case "findMinMax":
-            print("Min: ", z.find_min())
-            print("Max", z.find_max())
-        case "exit":
-            x= False
-        
+        tree_data.append(int(a[i]))
+    z=trees.BST(tree_data)
     
+    while(True):
+        action = input("action> ")
+        match action:
+            case "help":
+                print("help         Show this message")
+                print("print        Print the tree using in-order, pre-order, post-order")
+                print("remove       Remove elements of the tree")
+                print("delete       Delete the whole tree")
+                print("export       Export the tree to tickzpicture")
+                print("rebalance    Rebalance the tree")
+                print("findMinMax   Finding minimum and maximum in tree")
+                print("exit         Exits the program (the same as ctrl + D)")
+
+            case  "print":
+                tab = z.print()
+                print("pre-order: ", tab[0])
+                print("In-order: ", tab[1])
+                print("post-order: ", tab[2])
+
+            case "remove":
+                tab=[]
+                tmp=""
+                nodes_to_remove = input("remove> ")
+                for i in range (0, len(nodes_to_remove)):
+                    if nodes_to_remove[i] != " ":
+                        tmp = tmp+nodes_to_remove[i]
+                    if nodes_to_remove[i] == " " or i == len(nodes_to_remove)-1:
+                        tab.append(tmp)
+                        tmp = ""
+                for i in tab:
+                    z.delete(int(i))
+
+            case "delete":
+                z.del_all()
+                print("Tree has been deleted")
+
+            case "export":
+                pass
+
+            case "rebalance":
+                pass
+            case "findMinMax":
+                print("Min: ", z.find_min())
+                print("Max", z.find_max())
+            case "exit":
+                break
+
