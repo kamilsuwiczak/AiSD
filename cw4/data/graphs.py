96a360a1282f73234a6371970a14777faeaae999
Marcin <marcinkwa2@gmail.com>
2024-05-21 22:38:30 +0200

    Graph visualization

--- cw4/data/graphs.py
+++ cw4/data/graphs.py
@@ -1,6 +1,11 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Graph:
     def __init__(self):
         self.graph = {}
+
 
     def add_edge(self, u, v):
         if u not in self.graph:
@@ -11,23 +16,35 @@ class Graph:
         self.graph[u].append(v)
         self.graph[v].append(u)
 
+
     def print_graph(self):
         for vertex in self.graph:
             print(vertex, "->", " -> ".join(str(x) for x in self.graph[vertex]))
+ 
+    def generate_hamiltonian_graph(self, num_vertices, saturation):
+        if num_vertices < 3:
+            return
+        num_edges = int(num_vertices*(num_vertices-1)/2 * saturation)
+        if num_edges < num_vertices:
+            num_edges = num_vertices
+        vertices = list(range(1, num_vertices + 1))
+        random.shuffle(vertices)
+        for i in range(num_vertices - 1):
+            self.add_edge(vertices[i], vertices[i + 1])
 
-# Create a new graph
+        self.add_edge(vertices[num_vertices - 1], vertices[0])
+
+    def draw_graph(self):
+        G = nx.Graph()
+        for vertex in self.graph:
+            for edge in self.graph[vertex]:
+                G.add_edge(vertex, edge)
+        
+        nx.draw(G, with_labels=True, font_weight='bold')
+        plt.show()
+
 g = Graph()
-
-# Add edges to the graph
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.add_edge(5, 1)
-g.add_edge(3, 4)
-
-# Print the graph
+    
+g.generate_hamiltonian_graph(7)
+g.draw_graph()
 g.print_graph()
-xd = {}
-xd[1] = 2
-xd[3] = 5
-xd[2] = 4
-print(xd)
