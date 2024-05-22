b10b5e0bd578f3407147562d6a7e471a08e018da
Kamil <kamil.suwiczak@wp.pl>
2024-05-22 23:28:00 +0200

    Benchmark added

--- cw3/data/benchmark.sh
+++ cw3/data/benchmark.sh
@@ -21,7 +21,7 @@ $normal============================[Program Output]===================$teal
 EOF
             printf "$(more $tmpLogFile)" #printf allows for pretty colors
             tmpFile=$(mktemp) #saving to tmp file so it can be logged in follow up log
-            run_project="python3 ../../mit3r/03_Graphs/main.py -generate"
+            run_project="python3 /cw3/data/main.py -generate"
             #Please note that <(echo $instance_size) is unnecesarry for routines kahn_sort and tarjan_sort, but the program should ignore invalid `actions`.
             result=$(/usr/bin/time -f "%S|%M" $run_project < <(cat $representation $benchmark $routine <(echo $instance_size) <(echo exit)) 2>&1 >$tmpFile)
             mem=${result##*|}
