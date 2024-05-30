c1e6b691b4c4d1d442dba0da63ec2a5d36d0770f
Marcin <marcinkwa2@gmail.com>
2024-05-30 18:16:46 +0200

    wstępne generowanie grafu z cyklem

--- cw4/data/graphs.py
+++ cw4/data/graphs.py
@@ -28,23 +28,32 @@ class Graph:
         if num_edges < num_vertices:
             num_edges = num_vertices
         vertices = list(range(1, num_vertices + 1))
+        
         random.shuffle(vertices)
-        for i in range(num_vertices - 1):
+        for i in range(num_vertices - 1):               #generowanie cyklu Hamiltona
             self.add_edge(vertices[i], vertices[i + 1])
-
+            num_edges -= 1
         self.add_edge(vertices[num_vertices - 1], vertices[0])
+        num_edges -= 1
 
+        while num_edges > 0:
+            u = random.choice(vertices)
+            v = random.choice(vertices)
+            if u != v and v not in self.graph[u]:
+                self.add_edge(u, v)
+                num_edges -= 1
     def draw_graph(self):
         G = nx.Graph()
         for vertex in self.graph:
             for edge in self.graph[vertex]:
                 G.add_edge(vertex, edge)
-        
-        nx.draw(G, with_labels=True, font_weight='bold')
+        nx.draw(G, with_labels=True, font_weight='bold', node_size=1000, font_size=10, node_color='skyblue', edge_color='black', width=1.5, edge_cmap=plt.cm.Blues)
+        graph = plt.gcf()
+        graph.canvas.manager.set_window_title('Graph')
         plt.show()
 
 g = Graph()
     
-g.generate_hamiltonian_graph(7)
+g.generate_hamiltonian_graph(7, 0.5)
 g.draw_graph()
 g.print_graph()
