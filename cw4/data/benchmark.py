8768fd2600b354ac65dd5285537af9a9e91fe06d
Kamil <kamil.suwiczak@wp.pl>
2024-06-08 19:58:39 +0200

    updated benchmark.py

--- cw4/data/benchmark.py
+++ cw4/data/benchmark.py
@@ -1,5 +1,7 @@
 from graphs import Graph
 import os
+import sys
+import time
 
 
 
@@ -10,10 +12,27 @@ def main():
     if os.path.exists("benchmark_results") == False:
         os.mkdir("benchmark_results")
     
+    
 
     graph = Graph()
-    graph.generate_hamiltonian_graph(1025, 0.3)
-    graph.print_graph()
 
+    file1 = open("benchmark_results/generating_hamiltonian_graph.csv", "w")
+    file1.write("type_of_action, instance_size, time\n")
+    
+    file2 = open("benchmark_results/find_Euler_Hamilton.csv", "w")
+    file2.write("type_of_action, instance_size, time\n")
+    for i in range (0,2):
+        start_time=time.time()
+        graph.generate_hamiltonian_graph(2**i, 0.3)
+        finish_time = time.time() - start_time
+        file1.write(f"generating_hamiltonian_graph, {2**i}, {finish_time}\n")
+
+        start_time1 = time.time()
+        graph.find_eulerian_cycle()
+        finish_time1 = time.time()-start_time1
+        file2.write(f"finding_eulerian_cycle_in_hamiltonian_graph, {2**i}, {finish_time1}\n")
+
+    file1.close()
+    file2.close()
 if __name__ == "__main__":
     main()
